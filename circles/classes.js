969a76905a7ce714eb8fc9d1f8b511ea7e5830a8
nikll123@hotmail.com <nikll123@hotmail.com>
2021-12-16 23:50:13 +0200

    OOP inheritance

--- circles/classes.js
+++ circles/classes.js
@@ -1,41 +1,74 @@
-class Rectangle {
-    constructor(x, y, width, height) {
+class shape {
+    constructor(id, x, y, fillStyle) {
+        this.id = id;
         this.x = x;
         this.y = y;
+        this.fillStyle = fillStyle; 
+        this.classname = 'shape';
+    }
+
+    toString() {
+        return this.classname + ': id=' + this.id + ', fillStyle=' + this.fillStyle + ', x=' + this.x + ', y=' + this.y;
+    };
+}
+
+class shapeDraggable extends shape {
+    constructor(id, x, y, fillStyle) {
+        super(id, x, y, fillStyle);
+        this.classname = 'shapeDraggable';
+        this.isDragging = false;
+    }
+}
+
+class rectangle extends shapeDraggable {
+    constructor(id, x, y, width, height, fillStyle) {
+        super(id, x, y, fillStyle);
+        this.classname = 'rectangle';
         this.width = width;
         this.height = height;
-        this.isDragging = false;
-
-        this.render = function (ctx) {
-            ctx.save();
-
-            ctx.beginPath();
-            ctx.rect(this.x, this.y, this.width, this.height);
-            ctx.fillStyle = '#2793ef';
-            ctx.fill();
-
-            ctx.restore();
-        };
     }
+
+    render(ctx) {
+        ctx.save();
+        ctx.beginPath();
+        ctx.fillStyle = this.fillStyle;
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.fill();
+        ctx.restore();
+    };
+
+    toString() {
+        return super.toString() + ', width=' + this.width + ', height=' + this.height;
+    };
 }
 
-class Arc {
-    constructor(x, y, radius, radians) {
-        this.x = x;
-        this.y = y;
+class arc extends shapeDraggable {
+    constructor(id, x, y, radius, radians, fillStyle) {
+        super(id, x, y, fillStyle);
+        this.classname = 'arc';
         this.radius = radius;
         this.radians = radians;
-        this.isDragging = false;
+    }
 
-        this.render = function (ctx) {
-            ctx.save();
+    render(ctx) {
+        ctx.save();
+        ctx.beginPath();
+        ctx.fillStyle = this.fillStyle;
+        ctx.arc(this.x, this.y, this.radius, 0, this.radians, false);
+        ctx.fill();
+        ctx.restore();
+    };
 
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, this.radius, 0, this.radians, false);
-            ctx.fillStyle = '#2793ef';
-            ctx.fill();
+    toString() {
+        return super.toString() + ', radius=' + this.radius + ', radians=' + this.radians ;
+    };
 
-            ctx.restore();
-        };
+}
+
+class circle extends arc {
+    constructor(id, x, y, radius, fillStyle) {
+        super(id, x, y, radius, 2 * Math.PI, fillStyle) ;
+        this.classname = 'circle';
     }
 }
+

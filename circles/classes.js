94c1172fdc22a14857beed73f8480e428c7d21b2
nikll123@hotmail.com <nikll123@hotmail.com>
2021-12-16 22:28:48 +0200

    objects converted to classes

---
+++ circles/classes.js
@@ -0,0 +1,41 @@
+class Rectangle {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.isDragging = false;
+
+        this.render = function (ctx) {
+            ctx.save();
+
+            ctx.beginPath();
+            ctx.rect(this.x - this.width * 0.5, this.y - this.height * 0.5, this.width, this.height);
+            ctx.fillStyle = '#2793ef';
+            ctx.fill();
+
+            ctx.restore();
+        };
+    }
+}
+
+class Arc {
+    constructor(x, y, radius, radians) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.radians = radians;
+        this.isDragging = false;
+
+        this.render = function (ctx) {
+            ctx.save();
+
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.radius, 0, this.radians, false);
+            ctx.fillStyle = '#2793ef';
+            ctx.fill();
+
+            ctx.restore();
+        };
+    }
+}

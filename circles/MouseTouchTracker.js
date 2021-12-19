a5ad191a365033d198121e2e6e34fbe942375262
nikll123@hotmail.com <nikll123@hotmail.com>
2021-12-19 19:05:12 +0200

    refactoring MouseTouchTracker

---
+++ circles/MouseTouchTracker.js
@@ -0,0 +1,45 @@
+var MouseTouchTracker = function (canvas, callback) {
+
+  function processEvent(evt) {
+    var rect = canvas.getBoundingClientRect();
+    var offsetTop = rect.top;
+    var offsetLeft = rect.left;
+
+    if (evt.touches) {
+      return {
+        x: evt.touches[0].clientX - offsetLeft,
+        y: evt.touches[0].clientY - offsetTop
+      }
+    } else {
+      return {
+        x: evt.clientX - offsetLeft,
+        y: evt.clientY - offsetTop
+      }
+    }
+  }
+
+  function onDown(evt) {
+    evt.preventDefault();
+    var coords = processEvent(evt);
+    callback('down', coords.x, coords.y);
+  }
+
+  function onUp(evt) {
+    evt.preventDefault();
+    callback('up');
+  }
+
+  function onMove(evt) {
+    evt.preventDefault();
+    var coords = processEvent(evt);
+    callback('move', coords.x, coords.y);
+  }
+
+  canvas.ontouchmove = onMove;
+  canvas.onmousemove = onMove;
+
+  canvas.ontouchstart = onDown;
+  canvas.onmousedown = onDown;
+  canvas.ontouchend = onUp;
+  canvas.onmouseup = onUp;
+}
